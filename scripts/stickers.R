adff799763793874fb61bcad5e83ba919f1f8ea5
Christian <christian.knudsen@gmail.com>
2024-03-23 15:02:03 +0100

    Hexstickers tilføjet.

---
+++ scripts/stickers.R
@@ -0,0 +1,38 @@
+library(hexSticker)
+
+# The Final Countdown
+
+imgurl <- "img/org/TFCD.webp"
+hexSticker::sticker(imgurl, package = "", 
+                    p_color =  "green",
+                    p_y = 1.6,
+                    h_fill = "black",
+                    h_color = "green",
+                    s_x = 1.0,
+                    s_y = 1.0,
+                    s_width = 1,
+                    url = "  TFCD",
+                    u_color = "green",
+                    u_size = 12,
+                    u_x = 0.95,
+                    u_y = 0.11,
+                    white_around_sticker = TRUE,
+                    filename = "img/TFCD.png")
+
+
+# Min egen pakke
+imgurl <- "img/org/KnudsenKit-transp.png"
+hexSticker::sticker(imgurl, package = "", 
+                    p_color =  "orange",
+                    p_y = 1.6,
+                    h_fill = "black",
+                    h_color = "orange",
+                    s_x = 1.0,
+                    s_y = 1.0,
+                    s_width = 1,
+                    url = "  KnudsenKit",
+                    u_color = "orange",
+                    u_size = 12,
+                    u_x = 0.95,
+                    u_y = 0.11,
+                    filename = "img/KnudsenKit.png")
