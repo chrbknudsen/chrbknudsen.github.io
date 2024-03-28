5ac0d72ffee59bd42f817367444dda7b3a2439c3
Christian <christian.knudsen@gmail.com>
2024-03-28 11:13:10 +0100

    euler hex

--- scripts/stickers.R
+++ scripts/stickers.R
@@ -1,5 +1,27 @@
 library(hexSticker)
 
+
+# euler
+imgurl <- "img/org/euler.webp"
+hexSticker::sticker(imgurl, package = "", 
+                    p_color =  "blue",
+                    p_y = 1.6,
+                    h_fill = "black",
+                    h_color = "blue",
+                    s_x = 1.0,
+                    s_y = 1.0,
+                    s_width = 1,
+                    url = "  euler",
+                    u_color = "blue",
+                    u_size = 12,
+                    u_x = 0.95,
+                    u_y = 0.11,
+                    white_around_sticker = TRUE,
+                    filename = "img/euler.png")
+
+
+
+
 # The Final Countdown
 
 imgurl <- "img/org/TFCD.webp"
