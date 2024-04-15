59edac81664265df4b45d875570a2704412abe9b
Christian <christian.knudsen@gmail.com>
2024-04-15 20:44:02 +0200

    funktion til skabelse af ny note

---
+++ scripts/new_note.R
@@ -0,0 +1,22 @@
+new_note <- function(title){
+  dato <- format(Sys.time(), "%Y%m%d")
+  dato_header <- format(Sys.time(), "%Y-%m-%d")
+  
+  path <- paste0("notes/",dato,"-", title)
+  content <- c('---',
+    'title: "indsæt title"',
+  'subtitle: "indsæt subtitle"',
+  'author: "Christian Knudsen"',
+  'categories:',
+  '  - kategori 1',
+  '  - kategori 2',
+  paste0('date: ',dato_header),
+  'toc: true',
+  'image: ""',
+  '---')
+  dir.create(path)
+  fileConn<-file(paste0(path,"/","index.qmd"))
+  writeLines(content, fileConn)
+  close(fileConn)
+}
+new_note("asdilikhjf")
