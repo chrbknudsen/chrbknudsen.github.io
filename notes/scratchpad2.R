4beb28c46fb37f272e9562c3d885a20e179901a2
Christian <christian.knudsen@gmail.com>
2024-04-27 13:11:55 +0200

    noter

---
+++ notes/scratchpad2.R
@@ -0,0 +1,17 @@
+Grundlæggende pi-walk - men med polære koordinater. Det kan også noget
+
+
+https://archive.org/details/Math_Constants
+har - men til en del mere end vi har lyst til at plotte.
+Square root of 2 (Sqrt(2))
+Square root of 3 (Sqrt(3))
+Golden ratio
+e
+Pi
+Natural logarithm of 2 (Log(2))
+Natural logarithm of 3 (Log(3))
+Natural logarithm of 10 (Log(10))
+Apéry's constant (Zeta(3))
+Lemniscate constant (Lemniscate)
+Catalan's constant (Catalan)
+Euler-Mascheroni constant (Euler's Constant)
