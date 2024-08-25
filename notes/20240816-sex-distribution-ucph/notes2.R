97cc15c5134645009f62dd1a0108a5f820c47068
Christian <christian.knudsen@gmail.com>
2024-08-25 16:47:34 +0200

    videre

--- notes/20240816-sex-distribution-ucph/notes2.R
+++ notes/20240816-sex-distribution-ucph/notes2.R
@@ -1,5 +1,16 @@
 
+head(fak_data)
 
+fak_data %>% 
+  select(-bestand) %>% 
+  pivot_wider(names_from = "køn", values_from = "andel") %>% 
+    mutate(across(c(4,5), ~ . - 50)) %>% 
+  pivot_longer(cols = diff:Mænd, values_to = "andel", names_to = "køn") %>% 
+  ggplot(aes(år, andel, color = køn)) +
+  geom_point() +
+  geom_line() +
+  facet_wrap(~fakultet) +
+  ylim(c(-60,60))
 
 
 fak_data %>%   
@@ -18,3 +29,25 @@ ggplot(aes(år,andel, color = fct_relevel(køn, c("Kvinder", "Mænd", "diff"))))
                      labels = c("diff" = "Abs. Forskel (%-point)", "Kvinder" = "Kvinder", "Mænd" = "Mænd")) +
   labs(caption = "Udviklingen i kønsfordelingen på KUs fakulteter 2014-2023
           Rå data: https://us.ku.dk/studiestatistik/studiestatistikker/bestand/")
+
+
+
+
+fak_data %>% 
+  filter(køn == "diff") %>% 
+  select(-bestand) %>% 
+  mutate(farve = factor(sign(andel))) %>% 
+   ggplot(aes(år, andel, group = fakultet, color = farve)) +
+  geom_point()+
+  geom_line() +
+  facet_wrap(~fakultet) +
+  ggtitle(label = "Forskel på andelen af kvinder og mænd",
+          subtitle = "En værdi på 50% svarer til en 70-20 fordeling i kvinders favør") +
+    labs(caption = "Udviklingen i kønsfordelingen på KUs fakulteter 2014-2023
+          Rå data: https://us.ku.dk/studiestatistik/studiestatistikker/bestand/") +
+  ylab("Andel af studenterbestand (%)")+ 
+  xlab("") +
+  theme_bw() +
+  scale_color_manual(values = c("1" = "green", "-1" = "red"),
+                     labels = c("1" = "Alt ok", "-1" = "Problem"))
+
