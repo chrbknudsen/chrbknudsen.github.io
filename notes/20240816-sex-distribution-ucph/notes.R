8a8c78a7cb6be9599f866d5891863c5c6421d8c8
Christian B. Knudsen <cbk@kb.dk>
2024-09-16 10:02:11 +0200

    rebuild

--- notes/20240816-sex-distribution-ucph/notes.R
+++ notes/20240816-sex-distribution-ucph/notes.R
@@ -2,10 +2,15 @@ library(tidyverse)
 library(rvest)
 library(readxl)
 
+# her finder vi data
 url <- "https://us.ku.dk/studiestatistik/studiestatistikker/bestand/"
-data_sti <- "notes/20240816-sex-distribution-ucph/data/"  # relativ til projektet
-# indlæser siden
 
+# Her gemmer vi data
+data_sti <- "notes/20240816-sex-distribution-ucph/data/"  # relativ til projektet
+
+data_sti <- "data/"  # relativ til projektet
+
+# indlæser siden med data
 siden <-  read_html(url)
 
 # ekstraherer excel fil navne fra siden
@@ -15,6 +20,11 @@ excel_links <- siden %>%
   grep("\\.xlsx$", ., value = TRUE) %>% 
   basename()
 
+# disse filer har vi allerede
+eksisterende_filer <- list.files(path = data_sti, full.names = FALSE)
+
+
+filer_til_hentning <- base::setdiff(excel_links, eksisterende_filer)
 
 
 # funktion til at downloade excel filerne
@@ -22,38 +32,59 @@ download_bestand <- function(x){
   download.file(paste0(url, x), paste0("notes/20240816-sex-distribution-ucph/data/", x), mode = "wb")
 }
 
-# downloader excel filerne
-sapply(excel_links, download_bestand)
+# downloader excel filerne - hvis der er nogen der skal hentes.
+if(length(filer_til_hentning)>0){sapply(excel_links, download_bestand)}
 
-udvalg <- c("Københavns Universitet", "Bachelor", "Kandidat")
-"notes/20240816-sex-distribution-ucph/data/KU_BESTAND_2015.xlsx"
-ekstraher_overordnede_data <- function(sti){
-data <- read_xlsx(sti)
-år <- names(data)[1] %>% str_extract("\\d{4}")
-data %>% 
-  rename(var = 1) %>% 
-  filter(var %in% udvalg | str_detect(var, "Fakultet")) %>% 
-  mutate(fakultet = case_when(
-    var == "Københavns Universitet" ~ "Alle",
-    str_detect(var, "Fakultet") ~ var,
-    .default = NA
-  )) %>%
-  mutate(år = år) %>% 
-  fill(fakultet, .direction = "down") %>% 
-  distinct() %>% 
-  mutate(var = case_when(
-    var == "Københavns Universitet" ~ "Alle",
-    str_detect(var, "Fakultet") ~ var,
-    .default = var  )) %>% 
-  select(år, fakultet,
-         niveau = var, 
-          Kvinder, Mænd, Total) %>% 
-  mutate(år = as.numeric(år))
+
+
+# funktion til at ekstrahere data fra excelfilerne.
+ekstraher_data <- function(fil){
+  data <- read_xlsx(fil)
+  år <- names(data)[1] %>% str_extract("\\d{4}")
+  
+  data %>% 
+    rename(fag = 1) %>% 
+    mutate(fakultet = case_when(
+      fag == "Københavns Universitet" ~ "Alle",
+      str_detect(fag, "Fakultet") ~ fag,
+      .default = NA
+    )) %>% 
+    fill(fakultet, .direction = "down") %>% 
+    filter(fakultet != "Alle") %>%
+    filter(!str_detect(fag, "Fakultet")) %>% 
+    mutate(niveau = case_when(
+      fag == "Bachelor" ~ fag,
+      fag == "Kandidat" ~ fag,
+      .default = NA
+    )) %>% 
+    fill(niveau, .direction = "down") %>% 
+    filter(!(fag %in% c("Bachelor","Kandidat"))) %>% 
+    select(-Total) %>% 
+    mutate(år = as.numeric(år))
+  
 }
 
-list.files(path = data_sti, full.names = TRUE) %>% as_tibble() %>% 
-  mutate(data = map(value, ekstraher_overordnede_data))
+# ekstraherer data fra alle excelfilerne
+data <- list.files(path = data_sti, full.names = TRUE) %>% as_tibble() %>% 
+  mutate(data = map(value, ekstraher_data))
+
+# unnest data til langt format
+data <- data %>% 
+  select(-value) %>% 
+  unnest(cols = data) %>% 
+  pivot_longer(Kvinder:Mænd, names_to = "køn", values_to = "bestand")
 
 
-# og så er der noget galt i deres regneark for 2022. der sniger sig en ekstra
-kandidat ind.
+data %>% 
+  pivot_wider(names_from = køn, values_from = bestand) %>% 
+  mutate(samlet = Kvinder + Mænd) %>% 
+  select(-fag) %>% 
+  group_by(fakultet, år) %>% 
+  summarise(Kvinder = sum(Kvinder),
+            Mænd = sum(Mænd),
+            samlet = sum(samlet)) %>% 
+  ungroup() %>% 
+  mutate(Kvinder = Kvinder/samlet*100,
+         Mænd = Mænd/samlet*100,
+         Diff = Kvinder - Mænd) %>% 
+  select(-samlet)
