d15a149ddfec99e1097c4b0fcce714eac174421c
Christian Knudsen <cbk@kb.dk>
2025-08-22 10:30:59 +0200

    initial commit

--- scripts/stickers.R
+++ scripts/stickers.R
@@ -14,6 +14,26 @@ make_transparent <- function(sti, fuzz = 50){
   image_write(path = sti)
 }
 
+# kogebog
+imgurl <- "img/org/kogebog.png"
+hexSticker::sticker(imgurl, package = "", 
+                    p_color =  "black",
+                    p_y = 1.6,
+                    h_fill = "white",
+                    h_color = "darkred",
+                    s_x = 1.0,
+                    s_y = 1.0,
+                    s_width = 1.1,
+                    url = "Kogebog",
+                    u_color = "darkred",
+                    u_size = 11,
+                    u_x = 0.95,
+                    u_y = 0.11,
+                    #        u_angle = 45,
+                    white_around_sticker = TRUE,
+                    filename = "img/kogebog.png")
+make_transparent("img/kogebog.png", fuzz = 20)
+
 # project-euler
 imgurl <- "img/org/euler.png"
 hexSticker::sticker(imgurl, package = "", 
