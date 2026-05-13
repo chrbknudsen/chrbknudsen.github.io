3f080e0af601ac8069802bc896326a36207aa1a2
Christian Knudsen <cbk@kb.dk>
2026-05-13 12:26:01 +0200

    install-note tilføjet

--- scripts/build_site.R
+++ scripts/build_site.R
@@ -1,5 +1,9 @@
 # Det er denne der faktisk renderer siden
 babelquarto::render_website()
 
+# Den installeres ved
+# install.packages('babelquarto', repos = c('https://ropensci.r-universe.dev', 'https://cloud.r-project.org'))
+
+
 # Skal vi se resultat:
 # servr::httw("docs")
